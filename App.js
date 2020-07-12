f4f10f6c57889202d05b9e40272e0e9c761eab38
ParthKejriwal <62254837+ParthKejriwal@users.noreply.github.com>
2020-07-12 13:37:11 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation/bottom-tabs';
-import WriteScreen from './WriteScreen';
-import ReadScreen from './ReadScreen';
 
-export default class App extends React.Component{
-  render(){
-    return(
-      <AppContainer />
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
-
-const TabNavigator = createBottomTabNavigator({
-  WriteScreen: {screen: WriteScreen},
-  ReadScreen: {screen: ReadScreen},
-  });
-const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
